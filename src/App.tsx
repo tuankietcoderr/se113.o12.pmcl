3b5e5d47a0ba33f08b0215053e6e0558fc64058b
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2023-12-18 00:18:08 +0700

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import { cn } from "./lib/utils";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className={cn("text-red-100")}>Tekblog</h1>
+    </>
+  );
+}
+
+export default App;
